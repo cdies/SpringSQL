e741c4d950514806c8f64d56739c521fc068c9b5
m.pervushin <mihail.pervushin@kupivip.ru>
2017-12-29 15:54:59 +0300

    Logger

--- src/main/java/ru/sql/proj/controller/CustomerController.java
+++ src/main/java/ru/sql/proj/controller/CustomerController.java
@@ -1,5 +1,8 @@
 package ru.sql.proj.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,14 +10,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sql.proj.model.Order;
 import ru.sql.proj.service.OrderService;
-
 import java.util.Collection;
+
 
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
 
     private static final Logger LOG = LoggerFactory.getLogger(CustomerController.class);
+
+    private ObjectMapper mapper = new ObjectMapper();
 
     @Autowired
     private OrderService orderService;
@@ -23,6 +28,7 @@ public class CustomerController {
     @ResponseBody
     Collection<Order> get() throws Exception {
         Collection<Order> customers = orderService.exportChanges();
+        LOG.info("Testing logstash!!!!" + mapper.writeValueAsString(customers));
         return customers;
     }
 }
